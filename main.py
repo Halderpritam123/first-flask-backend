590cd09053e82e8830110f1980c4679200dd46eb
Pritam Halder <115460430+Halderpritam123@users.noreply.github.com>
2023-07-08 09:33:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+# List to store the posted data
+posted_data = []
 
+@app.route('/', methods=['GET'])
+def get_data():
+    # Perform any necessary logic to retrieve data
+    data = {'message': 'This is a GET request'}
+    return jsonify(data)
+
+@app.route('/post_data', methods=['POST'])
+def post_data():
+    # Retrieve data from the request body
+    data = request.get_json()
+
+    # Perform any necessary logic with the data
+    # ...
+
+    # Append the posted data to the list
+    posted_data.append(data)
+
+    # Return a response
+    response = {'message': 'This is a POST request', 'data': data}
+    return jsonify(response)
+
+@app.route('/all_posted_data', methods=['GET'])
+def all_posted_data():
+    return jsonify(posted_data)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=False,host='0.0.0.0')
